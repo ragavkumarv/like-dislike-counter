f0b06707d18c3813e2bd6c9a9227879a659c108e
ragavkumarv <ragavkumarv@gmail.com>
2021-06-30 20:52:17 +0530

    Map, add to list, destructure

--- src/App.js
+++ src/App.js
@@ -3,16 +3,58 @@ import "./styles.css";
 
 // jsx - js extended
 // App component
-export default function App(props) {
-  console.log(props);
+export default function App() {
+  const intialPoll = [
+    { company: "Apple", color: "white" },
+    { company: "Samsung", color: "skyblue" },
+    { company: "MI", color: "orange" },
+    { company: "Oneplus", color: "red" },
+    { company: "Moto", color: "grey" }
+  ];
+  // react tracking then renders
+  const [poll, setPoll] = useState(intialPoll);
+
+  const [company, setCompany] = useState("");
+  const [color, setColor] = useState("");
+
+  const addContestant = () => setPoll(poll.concat({ company, color }));
+
+  // typing -> onChange - trigger -> event.target.value (contains new value)
+  // -> setCompany -> company
+  // -> setColor -> color
+
+  return (
+    <div>
+      <input
+        onChange={(event) => setCompany(event.target.value)}
+        placeholder="Enter company"
+      />
+      {/* BG color must change when you type in the input */}
+      <input
+        style={{ color }}
+        onChange={(event) => setColor(event.target.value)}
+        placeholder="Enter color"
+      />
+      <button onClick={addContestant}>+Add</button>
+      {poll.map((detalil) => (
+        <Vote company={detalil.company} color={detalil.color} />
+      ))}
+    </div>
+  );
+}
+// Vote is child component of App
+// Which is the child component of Vote? Counter & Content
+
+function Vote({ color, company }) {
+  // console.log(props);
   // state - data - likes
   // ternary operator used
   // const bgStyle = { backgroundColor: likes >= dislikes ? "green" : "crimson" };
   const bgStyle = { backgroundColor: "#eee" };
 
   return (
-    <div className="App" style={bgStyle}>
-      <h4 style={{ color: props.color }}>{props.company}</h4>
+    <div className="vote-system" style={bgStyle}>
+      <h4 style={{ color }}>{company}</h4>
       <Counter color="orchid" emoji="👍" />
       {/* ctrl+/ */}
       {/* Task is to refactor the dislike button using Counter component */}
@@ -21,25 +63,54 @@ export default function App(props) {
     </div>
   );
 }
-// DRY - Dont Repeat Yourself
 
+// DRY - Dont Repeat Yourself
 // Clue  - Convert color & emoji as props
 // common - counter , different - thumbs up/down, color - green,red
 // First letter must be capital
 function Content() {
-  return <h3> Cast your Votes </h3>;
+  const [expanded, setExpanded] = useState(true); // true
+  // condtional rendering
+  return (
+    <div style={{ marginTop: "10px" }}>
+      <button onClick={() => setExpanded(!expanded)}>
+        Show {expanded ? "Less" : "More"}
+      </button>
+      {expanded ? <p> Add content </p> : ""}
+    </div>
+  );
 }
 
-function Counter(props) {
+//1. Task
+// Each company different content
+// Adding company you add with a content
+
+//2. Task
+// Build show more recipe
+// map - loop all the recipes
+// Add recipes
+
+// Show More -> Show Less -> Show More
+// expanded === true ? false : true;
+// !expanded
+// true -> click -> false -> click -> true ....
+
+// 10 > 7 ? 'Awesome' : 'cool';
+
+// function Counter(props) {
+function Counter({ color, emoji }) {
+  // Destructure props
+  // const {color, emoji} = props;
   const [counter, setCounter] = useState(0); // React hook
   const counterStyles = {
     fontSize: "25px",
     fontWeight: "bold",
-    color: props.color
+    color
   };
   return (
     <button style={counterStyles} onClick={() => setCounter(counter + 1)}>
-      {props.emoji} {counter}
+      {/* <button style={counterStyles} onClick={() => counter = counter + 1}> */}
+      {emoji} {counter}
     </button>
   );
 }
